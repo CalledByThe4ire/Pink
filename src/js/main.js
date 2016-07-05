a3569dbe59a2972794671ee68c893e49cc250c8c
Andrey Karamnoff <karamnoff@gmail.com>
2016-07-05 12:08:48 +0300

    initial commit

---
+++ src/js/main.js
@@ -0,0 +1,12 @@
+//= ../../bower_components/jquery/dist/jquery.js
+//= ../../bower_components/html5shiv/dist/html5shiv.js
+//= ../../bower_components/picturefill/dist/picturefill.js
+//= ../../bower_components/object-fit-images/dist/ofi.browser.js
+
+
+//= partials/object-fit-images.js
+//= partials/nav-toggle.js
+//= partials/map.js
+//= partials/feedback-form_text-field-mod.js
+//= partials/editor-form-mod.js
+//= partials/feedback-form_submit-check.js
